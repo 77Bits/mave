c65e8ae05d8894e687bfb8fc940addb7db68ce3a
77Bits <77Bits@proton.me>
2025-08-23 21:04:05 +0000

    feat: Add image caching

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:mave/typs.dart';
+import 'package:mave/pages/widgets/comic_grid.dart' show ComicGrid;
+
+
+
 
 //test libs
 import 'package:http/http.dart' as http;
 import 'package:mave/website/scraper.dart';
 //
+
+
 
 
 
@@ -47,58 +53,4 @@ class FirstTimeLoading extends StatelessWidget {
     return Scraper.comics(resp.body).toList();
   }
 
-}
-
-
-class ComicGrid extends StatelessWidget {
-  final List<Comic> _comicList;
-
-
-  const ComicGrid(this._comicList, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 150,
-        mainAxisSpacing:4,
-        crossAxisSpacing: 4,
-        childAspectRatio: 0.7,
-      ),
-      itemCount: _comicList.length,
-      itemBuilder: (context, index){
-        var cm = _comicList[index];
-        return ClipRRect(
-          borderRadius: BorderRadius.circular(7.14),
-          child: ColoredBox(
-            color: Colors.green,
-            child:Column(
-              mainAxisSize:MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Flexible(
-                  child: Image.network(
-                    cm.cover,
-                    fit: BoxFit.cover,
-                    height: double.infinity,
-                    width: double.infinity,
-                  )
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 3.3, vertical: 0.8),
-                  child: Text(
-                    cm.title,
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    textAlign: TextAlign.left,
-                  )
-                )
-              ],
-            )
-          )
-        );
-      },
-    );
-  }
-  
 }
