aff210b52cb77e22907766349f0d8f48dca930f7
77Bits <77Bits@proton.me>
2025-08-26 15:57:57 +0000

    feat(gui): Better NetworkError handling in gui (home page)

--- lib/toolkit/fetch/load_comics.dart
+++ lib/toolkit/fetch/load_comics.dart
@@ -5,15 +5,17 @@ import 'package:mave/website/scraper.dart';
 
 
 
-
 Future<List<Comic>> loadComics(int page) async{
-  final comicsFromDb = await ComicDb.self.getComicsWithOffset(page);
-  if (page == 0 && comicsFromDb.length == 0){
+  return await ComicDb.self.getComicsWithOffset(page);
+}
+
+
+
+Future<void> fillEmptyDatabase() async{
+  final comicsFromDb = await ComicDb.self.getComicsWithOffset(0);
+  if (comicsFromDb.length == 0){
     final resp = await http.get(Uri.parse("https://mangapill.com/chapters"));
     final comicsFromRemote = Scraper.comics(resp.body).toList();
     await ComicDb.self.addComics(comicsFromRemote);
-    return await ComicDb.self.getComicsWithOffset(page);
   }
-  else 
-    return comicsFromDb;
 }
