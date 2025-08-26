815853eeef0e31a202c49ef12c510a77b551af7f
null7u <7null@proton.me>
2025-08-26 12:25:58 +0000

    feat(db): Add db for comics

--- lib/toolkit/typs.dart
+++ lib/toolkit/typs.dart
@@ -3,21 +3,43 @@
 
 
 class Comic {
+
+  static const int BOOKMARKED = 2;
+  static const int BLOCKED    = 1;
+  static const int NORMAL     = 0;
+
+
+
+  int? id;
+
   final String title;
   final String url;
   final String cover;
+  final int    state;
 
 
   
   Comic({
+    this.id,
     required this.title,
     required this.url, 
     required this.cover,
+    this.state = 0,
   });
 
 
   @override
   String toString() {
     return title;
+  }
+
+
+  Map<String, dynamic> toMap(){
+    return {
+      "title":title,
+      "url"  :url,
+      "cover":cover,
+      "state":state,
+    };
   }
 }
